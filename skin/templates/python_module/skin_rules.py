46d8206932b2fb9e847e67490ff1ac47f973c8d7
leiserfg <leiserfg@gmail.com>
2015-11-16 14:37:09 -0500

    best unicode suport

--- skin/templates/python_module/skin_rules.py
+++ skin/templates/python_module/skin_rules.py
@@ -7,6 +7,6 @@ import getpass
 fullname = prompt('Full module name (as package.module): ')
 name = fullname.split('.')[-1]
 description = prompt('Package description: ')
-year = prompt(u"(c) Year: ", date.today().year)
+year = prompt(u"© Year: ", date.today().year)
 author = prompt("Author: ", getpass.getuser())
 license = prompt('Project licence', 'BSD License')
