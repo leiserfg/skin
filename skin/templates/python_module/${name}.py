9e89aa5ac26943dc7cd2c4c67faab8bcd3ec2745
leiserfg <leiserfg@gmail.com>
2015-11-16 11:15:48 -0500

    working on new skin

---
+++ skin/templates/python_module/${name}.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    $fullname
+    <%for l in name%>~<%endfor%>
+
+    $description
+
+    :copyright: (c) $year by $author
+    :license: BSD License.
+"""
---
+++ skin/templates/python_module/skin_rules.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+import getpass 
+
+
+fullname = prompt('Full module name (as package.module)')
+name = fullname.split('.')[-1]
+description = prompt('Package description')
+year = prompt(u"© Year", date.today().year)
+author = prompt("Author", getpass.getuser())
